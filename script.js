155d442808d87c9b8275ae517ba3c162b0737c53
A111fault <troy.afif7@email.com>
2025-05-09 01:45:48 +0600

    java script code mostly done need to fix errors

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ function connect() {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => 
-            showInBrowser(data));
+        .then(data => {
+            showInBrowser(data);
+            document.getElementById("searchBox").value = ""; 
+        });
+}
+
+
+function showInBrowser(data) {
+    var displayArea = document.getElementById("displayArea");
+    displayArea.textContent = "";
+
+    var country = data[0]; // show only the first result
+
+    var div = document.createElement("div");
+    div.className = "countryBox";
+
+    
+    
+
+    div.innerHTML = `
+         <img src="${country.flags.svg}" width="200" alt="Flag"><br><br>
+        <strong>Country Name:</strong> ${country.name.common} <br>
+        <strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"} <br>
+        <strong>Region:</strong> ${country.region} <br>
+        <strong>Subregion:</strong> ${subregion} <br>
+       
+    `;
+
+    displayArea.appendChild(div);
 }
